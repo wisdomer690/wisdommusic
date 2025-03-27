e45c8d9c112122144999aa574f996554583fd775
razor23gtk <razorclaw996@gmail.com>
2025-03-27 21:31:05 +0000

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "Avinan",
-      password: "avinan",
-      host: "new-york-node-1.vortexcloud.xyz",
-      port:  9323,
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  9372,
       secure: false
     }
   ]
