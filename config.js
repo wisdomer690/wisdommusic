ee5e236219d43a36799682e535c07da5931d3d43
wisdomer690 <c.arljhon879@gmail.com>
2025-04-28 03:51:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,12 +16,12 @@ module.exports = {
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-     {
-      name: "Avinan",
-      password: "avinan",
-      host: "new-york-node-1.vortexcloud.xyz",
-      port:  5008,
+  {
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  9372,
       secure: false
-    }
+    
   ]
 }
